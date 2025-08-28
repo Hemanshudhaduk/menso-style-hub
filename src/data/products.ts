e82d60ef6419d77e69128a5e296027fb482873dc
Hemanshudhaduk <115709310+Hemanshudhaduk@users.noreply.github.com>
2025-08-28 19:35:59 +0530

    done

--- src/data/products.ts
+++ src/data/products.ts
@@ -23,6 +23,104 @@ import h7w1v_2 from '@/assets/2h7w1v/2h7w1v_2.webp';
 import h7w1v_3 from '@/assets/2h7w1v/2h7w1v_3.webp';
 import h7w1v_4 from '@/assets/2h7w1v/2h7w1v_4.webp';
 
+
+import cbki1 from '@/assets/63cbki/63cbki_1.webp';
+import cbki2 from '@/assets/63cbki/63cbki_2.webp';
+import cbki3 from '@/assets/63cbki/63cbki_3.webp';
+import cbki4 from '@/assets/63cbki/63cbki_4.webp';
+
+
+import frcm1 from '@/assets/77frcm/77frcm_1.webp';
+import frcm2 from '@/assets/77frcm/77frcm_1.webp';
+import frcm3 from '@/assets/77frcm/77frcm_3.webp';
+import frcm4 from '@/assets/77frcm/77frcm_4.webp';
+
+
+import mg7nd1 from '@/assets/4mg7nd/4mg7nd_1.webp';
+import mg7nd2 from '@/assets/4mg7nd/4mg7nd_2.avif';
+import mg7nd3 from '@/assets/4mg7nd/4mg7nd_3.webp';
+import mg7nd4 from '@/assets/4mg7nd/4mg7nd_4.avif';
+
+
+import jtmad1 from '@/assets/5jtmad/5jtmad_1.avif';
+import jtmad2 from '@/assets/5jtmad/5jtmad_2.webp';
+import jtmad3 from '@/assets/5jtmad/5jtmad_3.avif';
+import jtmad4 from '@/assets/5jtmad/5jtmad_4.avif';
+
+import sjykz1 from '@/assets/jtmad1/jtmad1__1.webp';
+import sjykz2 from '@/assets/jtmad1/jtmad1_2.webp';
+import sjykz3 from '@/assets/jtmad1/jtmad1_3.avif';
+import sjykz4 from '@/assets/jtmad1/jtmad1_4.avif';
+
+import wkcil1 from '@/assets/8wkcil/8wkcil_1.avif';
+import wkcil2 from '@/assets/8wkcil/8wkcil_2.avif';
+import wkcil3 from '@/assets/8wkcil/8wkcil_3.avif';
+import wkcil4 from '@/assets/8wkcil/8wkcil_4.webp';
+
+import d7i3r1 from '@/assets/6d7i3r/6d7i3r_1.avif';
+import d7i3r2 from '@/assets/6d7i3r/6d7i3r_2.avif';
+import d7i3r3 from '@/assets/6d7i3r/6d7i3r_3.avif';
+import d7i3r4 from '@/assets/6d7i3r/6d7i3r_4.avif'; 
+
+import tdmh1 from '@/assets/55tdmh/55tdmh_1.avif';
+import tdmh2 from '@/assets/55tdmh/55tdmh_2.avif';
+import tdmh3 from '@/assets/55tdmh/55tdmh_3.avif';
+import tdmh4 from '@/assets/55tdmh/55tdmh_4.avif';
+
+import tylxj1 from '@/assets/5tylxj/5tylxj_1.avif';
+import tylxj2 from '@/assets/5tylxj/5tylxj_2.avif';
+import tylxj3 from '@/assets/5tylxj/5tylxj_3.avif';
+import tylxj4 from '@/assets/5tylxj/5tylxj_4.webp';
+
+import ojpif from '@/assets/9ojpif/9ojpif_1.webp';
+
+
+import eqip1 from '@/assets/65eqip/65eqip.webp';
+import eqip2 from '@/assets/65eqip/65eqip_2.avif';
+import eqip3 from '@/assets/65eqip/65eqip_3.webp';
+import eqip4 from '@/assets/65eqip/65eqip_4.webp';
+
+
+import fg5c1 from '@/assets/86fg5c/86fg5c_1.webp';
+import fg5c2 from '@/assets/86fg5c/86fg5c_2.webp';
+import fg5c3 from '@/assets/86fg5c/86fg5c_3.avif';
+import fg5c4 from '@/assets/86fg5c/86fg5c_4.webp';
+
+
+import bsk3i1 from '@/assets/bsk3i/bsk3i_1.webp';
+import bsk3i2 from '@/assets/bsk3i/bsk3i_2.webp';
+import bsk3i3 from '@/assets/bsk3i/bsk3i_3.avif';
+import bsk3i4 from '@/assets/bsk3i/bsk3i_4.webp';
+
+import l71dg1 from '@/assets/9l71dg/9l71dg_1.webp';
+import l71dg2 from '@/assets/9l71dg/9l71dg_2.webp';
+import l71dg3 from '@/assets/9l71dg/9l71dg_3.webp';
+import l71dg4 from '@/assets/9l71dg/9l71dg_4.webp';
+
+import a65a61 from '@/assets/7a65a6/7a65a6_1.webp';
+import a65a62 from '@/assets/7a65a6/7a65a6_2.avif';
+import a65a63 from '@/assets/7a65a6/7a65a6_3.webp';
+import a65a64 from '@/assets/7a65a6/7a65a6_4.webp';
+
+
+import uot641 from '@/assets/7uot64/7uot64_1.avif';
+import uot642 from '@/assets/7uot64/7uot64_2.avif';
+import uot643 from '@/assets/7uot64/7uot64_3.avif';
+import uot644 from '@/assets/7uot64/7uot64_4.webp';
+
+import z49pf1 from '@/assets/4z49pf/4z49pf_1.webp';
+import z49pf2 from '@/assets/4z49pf/4z49pf_2.webp';
+import z49pf3 from '@/assets/4z49pf/4z49pf_3.avif';
+import z49pf4 from '@/assets/4z49pf/4z49pf_4.avif';
+
+import mpzxj1 from '@/assets/8mpzxj/8mpzxj_1.webp';
+import mpzxj2 from '@/assets/8mpzxj/8mpzxj_2.webp';
+import mpzxj3 from '@/assets/8mpzxj/8mpzxj_3.avif';
+import mpzxj4 from '@/assets/8mpzxj/8mpzxj_4.webp';
+
+
+
+
 export const products: Product[] = [
   {
     id: 1,
@@ -138,7 +236,7 @@ export const products: Product[] = [
   {
     id: 5,
     name: "Sarees",
-    price: 11,
+    price: 199,
     originalPrice: 3000,
     discount: 96,
     rating: 4.8,
@@ -250,6 +348,559 @@ export const products: Product[] = [
         "Fit Type : Regular",
         "Ocassion : Casual, Formal, Casual, Daily",
         "Packet contains : 1 readymade Kurti."
+      ],
+      pants: [],
+      dupatta: []
+    }
+  },
+  {
+    id: "63cbki",
+    name: "Aishani Fabulous Kurtis",
+    price: 99,
+    originalPrice: 258,
+    discount: 33,
+    rating: 3.9,
+    reviews: 1200,
+    image: cbki1,
+    images: [cbki1, cbki2, cbki3, cbki4],
+    category: "kurtis",
+    sizes: ["S", "M", "L", "XL", "XXL","XXXL"],
+    description: "Trendy Fabulous Kurtis ",
+    fabric: "Rayon",
+    care: "Hand Wash",
+    shipping: "5-7 Days",
+    details: {
+      kurta: [
+        "Fabric - Rayon",
+        "Anarkali Style full flare",
+        "Round Neck",
+        "Length - 50 inch",
+        "Sleeve Length - Three-Quarter Sleeves",
+        "Pattern- Printed", 
+        "Combo of : Single",
+        "Fit Type : Regular",
+        "Ocassion : Casual, Formal, Casual, Daily",
+        "Packet contains : 1 readymade Kurti."
+      ],
+      pants: [],
+      dupatta: []
+    }
+  },
+  {
+    id: "77frcm",
+    name: "Charvi Fashionable Kurtis",
+    price: 99,
+    originalPrice: 259,
+    discount: 33,
+    rating: 3.4,
+    reviews: 9490,
+    image: frcm1,
+    images: [frcm1, frcm2, frcm3, frcm4],
+    category: "kurtis",
+    sizes: ["S", "M", "L", "XL", "XXL","XXXL"],
+    description: "Kurtis for women -2024 Collection",
+    fabric: "Satin",
+    care: "Hand Wash",
+    shipping: "5-7 Days",
+    details: {
+      kurta: [
+        "Fabric - Satin",
+        "Anarkali Style full flare",
+        "Round Neck",
+        "weight - 300 gm",  
+        "Length - 50 inch",
+        "Sleeve Length - Short Sleeves",
+        "Pattern- Printed", 
+        "Combo of : Single",
+      ],
+      pants: [],
+      dupatta: []
+    }
+  },
+  {
+    id: "4mg7nd",
+    name: "Polyster/Crepe fabric-jivika superior Kurti",
+    price: 99,
+    originalPrice: 290,
+    discount: 33,
+    rating: 3.8,
+    reviews: 1757,
+    image: mg7nd1,
+    images: [mg7nd1, mg7nd2, mg7nd3, mg7nd4],
+    category: "kurtis",
+    sizes: ["S", "M", "L", "XL", "XXL","XXXL"],
+    description: "kurti palazzo set chikankari kurti kurti set short kurti",
+    fabric: "Crepe",
+    care: "Hand Wash",
+    shipping: "5-7 Days",
+    details: {
+      kurta: [
+        "Fabric - Crepe",
+        "Anarkali Style full flare",
+        "Round Neck",
+        "weight - 300 gm",  
+        "Length - 50 inch",
+        "Sleeve Length - Three-Quarter Sleeves",
+        "Pattern- Checked", 
+        "Combo of : Single",
+      ],
+      pants: [],
+      dupatta: []
+    }
+  },
+  {
+    id: "5jtmad",
+    name: "Anarkali High Demand trendy Rayon Gown Kurtis",
+    price: 99,
+    originalPrice: 240,
+    discount: 33,
+    rating: 3.8,
+    reviews: 7751,
+    image: jtmad1,
+    images: [jtmad1, jtmad2, jtmad3, jtmad4],
+    category: "kurtis",
+    sizes: ["S", "M", "L", "XL", "XXL","XXXL"],
+    description: "women, men and children. Customers can expect a world-class shopping environment stocking the latest in international fashion from around the globe, as well as DDG's own label of in-house designs in carefully chosen color palettes.",
+    fabric: "Rayon",
+    care: "Hand Wash",
+    shipping: "5-7 Days",
+    details: {
+      kurta: [
+        "Fabric - Rayon",
+        "Anarkali Style full flare",
+        "Round Neck",
+        "weight - 300 gm",  
+        "Length - 50 inch",
+        "Sleeve Length - Three-Quarter Sleeves",
+        "Pattern- Solid", 
+        "Combo of : Single",
+      ],
+      pants: [],
+      dupatta: []
+    }
+  },
+  {
+    id: "sjykz1",
+    name: "ryon kurtis",
+    price: 99,
+    originalPrice: 249,
+    discount: 33,
+    rating: 4.0,
+    reviews: 4000,
+    image: sjykz1,
+    images: [sjykz1, sjykz2, sjykz3, sjykz4],
+    category: "kurtis",
+    sizes: ["S", "M", "L", "XL", "XXL","XXXL"],
+    description: "women, men and children. Customers can expect a world-class shopping environment stocking the latest in international fashion from around the globe, as well as DDG's own label of in-house designs in carefully chosen color palettes.",
+    fabric: "Rayon",
+    care: "Hand Wash",
+    shipping: "5-7 Days",
+    details: {
+      kurta: [
+        "Fabric - Rayon",
+        "Anarkali Style full flare",
+        "Round Neck",
+        "weight - 300 gm",  
+        "Length - 50 inch",
+        "Sleeve Length -  Long Sleeves",
+        "Pattern- Chikankari", 
+        "Combo of : Single",
+      ],
+      pants: [],
+      dupatta: []
+    }
+  },
+  {
+    id: "8wkcil",
+    name: "Women's rayon printed short kurti",
+    price: 99,
+    originalPrice: 277,
+    discount: 4,
+    rating: 4.1,
+    reviews: 4336,
+    image: wkcil1,
+    images: [wkcil1, wkcil2, wkcil3, wkcil4],
+    category: "kurtis",
+    sizes: ["S", "M", "L", "XL", "XXL","XXXL"],
+    description: "kurti palazzo set chikankari kurti kurti set short kurti",
+    fabric: "Rayon",
+    care: "Hand Wash",
+    shipping: "5-7 Days",
+    details: {
+      kurta: [
+        "Fabric - Rayon",
+        "Anarkali Style full flare",
+        "Round Neck",
+        "weight - 300 gm",  
+        "Length - 50 inch",
+        "Sleeve Length - Long Sleeves",
+        "Pattern- Checked", 
+        "Combo of : Single",
+      ],
+      pants: [],
+      dupatta: []
+    }
+  },
+  {
+    id: "6d7i3r",
+    name: "SAHJANAND FASHIONABLE KURTIS",
+    price: 99,
+    originalPrice: 277,
+    discount: 4,
+    rating: 4.1,
+    reviews: 1340,
+    image: d7i3r1,
+    images: [d7i3r1, d7i3r2, d7i3r3, d7i3r4],
+    category: "kurtis",
+    sizes: ["S", "M", "L", "XL", "XXL","XXXL"],
+    description: "kurti palazzo set chikankari kurti kurti set short kurti",
+    fabric: "Georgette",
+    care: "Hand Wash",
+    shipping: "5-7 Days",
+    details: {
+      kurta: [
+        "Fabric - Georgette",
+        "Anarkali Style full flare",
+        "Round Neck",
+        "weight - 300 gm",  
+        "Length - 50 inch",
+        "Sleeve Length - Three-Quarter Sleeves",
+        "Pattern- Chikankari", 
+        "Combo of : Single",
+      ],
+      pants: [],
+      dupatta: []
+    }
+  },
+  {
+    id: "55tdmh",
+    name: "Women Rayon Printed Aaradhya Anarkali Kurtis",
+    price: 99,
+    originalPrice: 295,
+    discount: 4,
+    rating: 4.0,
+    reviews: 10225,
+    image: tdmh1,
+    images: [tdmh1, tdmh2, tdmh3, tdmh4],
+    category: "kurtis",
+    sizes: ["S", "M", "L", "XL", "XXL","XXXL"],
+    description: "Buy Kurtas for Women Online at Best Price on Meesho",
+    fabric: "Rayon",
+    care: "Hand Wash",
+    shipping: "5-7 Days",
+    details: {
+      kurta: [
+        "Fabric - Rayon",
+        "Anarkali Style full flare",
+        "Round Neck",
+        "weight - 300 gm",  
+        "Length - 50 inch",
+        "Sleeve Length - Three-Quarter Sleeves",
+        "Pattern- Chikankari", 
+        "Combo of : Single",
+      ],
+      pants: [],
+      dupatta: []
+    }
+  },
+  {
+    id: "5tylxj",
+    name: "Anarkali Gown embroidry",
+    price: 99,
+    originalPrice: 300,
+    discount: 4,
+    rating: 3.9,
+    reviews: 3213,
+    image: tylxj1,
+    images: [tylxj1, tylxj2, tylxj3, tylxj4],
+    category: "kurtis",
+    sizes: ["S", "M", "L", "XL", "XXL","XXXL"],
+    description: "Buy Kurtas for Women Online at Best Price on Meesho",
+    fabric: "Rayon",
+    care: "Hand Wash",
+    shipping: "5-7 Days",
+    details: {
+      kurta: [
+        "Fabric - Rayon",
+        "Anarkali Style full flare",
+        "Round Neck",
+        "weight - 300 gm",  
+        "Length - 50 inch",
+        "Sleeve Length - Three-Quarter Sleeves",
+        "Pattern- Solid", 
+        "Combo of : Single",
+      ],
+      pants: [],
+      dupatta: []
+    }
+  },
+  {
+    id: "9ojpif",
+    name: "Women's Latest Floral Printed Kurta new collection",
+    price: 99,
+    originalPrice: 300,
+    discount: 33,
+    rating: 3.9,
+    reviews: 3213,
+    image: ojpif,
+    images: [],
+    category: "kurtis",
+    sizes: ["S", "M", "L", "XL", "XXL","XXXL"],
+    description: "Buy Kurtas for Women Online at Best Price on Meesho",
+    fabric: "Cotton",
+    care: "Hand Wash",
+    shipping: "5-7 Days",
+    details: {
+      kurta: [
+        "Fabric - Cotton",
+        " Women's Latest Floral Printed Kurta new collection",
+        "weight - 300 gm",  
+        "Length - 50 inch",
+        "Sleeve Length -  Sleeves",
+        "Pattern- Printed", 
+        "Combo of : Single",
+      ],
+      pants: [],
+      dupatta: []
+    }
+  },
+  {
+    id: "65eqip",
+    name: "Kurtis For Woman",
+    price: 99,
+    originalPrice: 300,
+    discount: 33,
+    rating: 4.2,
+    reviews: 7001,
+    image: eqip1,
+    images: [eqip1, eqip2, eqip3, eqip4],
+    category: "kurtis",
+    sizes: ["S", "M", "L", "XL", "XXL","XXXL"],
+    description: "Buy Kurtas for Women Online at Best Price on Meesho",
+    fabric: "Net",
+    care: "Hand Wash",
+    shipping: "5-7 Days",
+    details: {
+      kurta: [
+        "Fabric - Net",
+        " Women's Latest Floral Printed Kurta new collection",
+        "weight - 300 gm",  
+        "Length - 50 inch",
+        "Sleeve Length -  Three-Quarter Sleeves",
+        "Pattern- Solid", 
+        "Combo of : Single",
+      ],
+      pants: [],
+      dupatta: []
+    }
+  },
+  {
+    id: "86fg5c",
+    name: "Women's Trending Rayon Printed Black Short Kurti",
+    price: 99,
+    originalPrice: 300,
+    discount: 33,
+    rating: 4.2,
+    reviews: 22711,
+    image: fg5c1,
+    images: [fg5c1, fg5c2, fg5c3, fg5c4],
+    category: "kurtis",
+    sizes: ["S", "M", "L", "XL", "XXL","XXXL"],
+    description: "Buy Kurtas for Women Online at Best Price on Meesho",
+    fabric: "Rayon",
+    care: "Hand Wash",
+    shipping: "5-7 Days",
+    details: {
+      kurta: [
+        "Fabric - Rayon",
+        " Women's Latest Floral Printed Kurta new collection",
+        "weight - 300 gm",  
+        "Length - 50 inch",
+        "Sleeve Length -  Three-Quarter Sleeves",
+        "Pattern- Printed", 
+        "Combo of : Single",
+      ],
+      pants: [],
+      dupatta: []
+    }
+  },
+  {
+    id: "bsk3i",
+    name: "Rayon printed Anarkali Sleevless Long Kurti cum Gown",
+    price: 99,
+    originalPrice: 300,
+    discount: 33,
+    rating: 4.2,
+    reviews: 16900,
+    image: bsk3i1,
+    images: [bsk3i1, bsk3i2, bsk3i3, bsk3i4],
+    category: "kurtis",
+    sizes: ["S", "M", "L", "XL", "XXL","XXXL"],
+    description: "Buta Flower Rayon Printed Short Kurta Casual Short Kurti For Women",
+    fabric: "Rayon",
+    care: "Hand Wash",
+    shipping: "5-7 Days",
+    details: {
+      kurta: [
+        "Fabric - Rayon",
+        " Women's Latest Floral Printed Kurta new collection",
+        "weight - 300 gm",  
+        "Length - 50 inch",
+        "Sleeve Length -  Three-Quarter Sleeves",
+        "Pattern- Printed", 
+        "Combo of : Single",
+      ],
+      pants: [],
+      dupatta: []
+    }
+  },
+  {
+    id: "9l71dg",
+    name: "Rayon printed Anarkali Sleevless Long Kurti cum GowButa Flower Rayon Printed Short Kurtan",
+    price: 99,
+    originalPrice: 600,
+    discount: 66,
+    rating: 4.4,
+    reviews: 16900,
+    image: l71dg1,
+    images: [l71dg1, l71dg2, l71dg3, l71dg4],
+    category: "kurtis",
+    sizes: ["S", "M", "L", "XL", "XXL","XXXL"],
+    description: "women gown ,women trending gown, peach gown, trending gown, anarkali gown, Elegant gown, Feminine gown, Graceful gown",
+    fabric: "Rayon",
+    care: "Hand Wash",
+    shipping: "5-7 Days",
+    details: {
+      kurta: [
+        "Fabric - Rayon",
+        " Women's Latest Floral Printed Kurta new collection",
+        "weight - 300 gm",  
+        "Length - 50 inch",
+        "Sleeve Length -  Three-Quarter Sleeves",
+        "Pattern- Printed", 
+        "Combo of : Single",
+      ],
+      pants: [],
+      dupatta: []
+    }
+  },
+  {
+    id: "7a65a6",
+    name: "Women Trending Rayon Printed Short Kurta",
+    price: 99,
+    originalPrice: 600,
+    discount: 66,
+    rating: 4.2,
+    reviews: 6299,
+    image: a65a61,
+    images: [a65a61, a65a62, a65a63, a65a64],
+    category: "kurtis",
+    sizes: ["S", "M", "L", "XL", "XXL","XXXL"],
+    description: "women gown ,women trending gown, peach gown, trending gown, anarkali gown, Elegant gown, Feminine gown, Graceful gown",
+    fabric: "Rayon",
+    care: "Hand Wash",
+    shipping: "5-7 Days",
+    details: {
+      kurta: [
+        "Fabric - Rayon",
+        " Women's Latest Floral Printed Kurta new collection",
+        "weight - 300 gm",  
+        "Length - 50 inch",
+        "Sleeve Length -  Three-Quarter Sleeves",
+        "Pattern- Printed", 
+        "Combo of : Single",
+      ],
+      pants: [],
+      dupatta: []
+    }
+  },
+  {
+    id: "7uot64",
+    name: "V-Neack short Cotton kurti for women",
+    price: 99,
+    originalPrice: 600,
+    discount: 66,
+    rating: 4.0,
+    reviews: 13099,
+    image: uot641,
+    images: [uot641, uot642, uot643, uot644],
+    category: "kurtis",
+    sizes: ["S", "M", "L", "XL", "XXL","XXXL"],
+    description: "V-Neack short Cotton kurti for women.",
+    fabric: "Cotton",
+    care: "Hand Wash",
+    shipping: "5-7 Days",
+    details: {
+      kurta: [
+        "Fabric - Cotton",
+        " Women's Latest Floral Printed Kurta new collection",
+        "weight - 300 gm",  
+        "Length - 50 inch",
+        "Sleeve Length -  Three-Quarter Sleeves",
+        "Pattern- Printed", 
+        "Combo of : Single",
+      ],
+      pants: [],
+      dupatta: []
+    }
+  },
+  {
+    id: "4z49pf",
+    name: "anarakali kurti and dupatta pink",
+    price: 99,
+    originalPrice: 600,
+    discount: 66,
+    rating: 4.0,
+    reviews: 1711,
+    image: z49pf1,
+    images: [z49pf1, z49pf2, z49pf3, z49pf4],
+    category: "kurtis",
+    sizes: ["S", "M", "L", "XL", "XXL","XXXL"],
+    description: "Chitrarekha Attractive Women KurtI DUPATTA.",
+    fabric: "Cotton",
+    care: "Hand Wash",
+    shipping: "5-7 Days",
+    details: {
+      kurta: [
+        "Fabric - Cotton",
+        " Women's Latest Floral Printed Kurta new collection",
+        "weight - 300 gm",  
+        "Length - 50 inch",
+        "Sleeve Length -  Three-Quarter Sleeves",
+        "Set Type : KurtI With Dupatta",
+        "Pattern- Printed", 
+        "Combo of : Single",
+      ],
+      pants: [],
+      dupatta: []
+    }
+  },
+  {
+    id: "8mpzxj",
+    name: "KVS TOP BLACK",
+    price: 99,
+    originalPrice: 600,
+    discount: 66,
+    rating: 4.0,
+    reviews: 1711,
+    image: mpzxj1,
+    images: [mpzxj1, mpzxj2, mpzxj3, mpzxj4],
+    category: "kurtis",
+    sizes: ["S", "M", "L", "XL", "XXL","XXXL"],
+    description: "Chitrarekha Attractive Women KurtI DUPATTA.",
+    fabric: "Cotton",
+    care: "Hand Wash",
+    shipping: "5-7 Days",
+    details: {
+      kurta: [
+        "Fabric - Cotton",
+        " Women's Latest Floral Printed Kurta new collection",
+        "weight - 300 gm",  
+        "Length - 50 inch",
+        "Sleeve Length -  Three-Quarter Sleeves",
+        "Set Type : KurtI With Dupatta",
+        "Pattern- Printed", 
+        "Combo of : Single",
       ],
       pants: [],
       dupatta: []
